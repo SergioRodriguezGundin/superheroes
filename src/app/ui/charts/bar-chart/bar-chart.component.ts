0c5a5544112c33a6a59a19fa78cf426573d4c140
SergioGundin <sergio.gundin.rodriguez@gmail.com>
2024-04-01 12:02:03 +0200

    wip: bar chart updates

--- src/app/ui/charts/bar-chart/bar-chart.component.ts
+++ src/app/ui/charts/bar-chart/bar-chart.component.ts
@@ -13,25 +13,91 @@ export class BarChartComponent {
   @ViewChild('barChartContainer', { static: true })
   barChartContainer!: ElementRef;
 
-  private width: number = 0;
-
-  private height: number = 0;
+  data = input<{ name: string; value: number }[]>([]);
 
   chartSize = output<{ width: number; height: number }>();
 
-  data = input<{ name: string; value: number }[]>([]);
+  private svg: any;
 
+  private width: number = 185;
+
+  private height: number = 185;
+
+  private x: any;
+
+  private y: any;
+
+  hasChartUpdate: boolean = false;
 
   constructor() {
     effect(() => {
       const newChartData = this.data();
-      if (newChartData?.length) {
-        this.initializeChart();
+
+      if (this.hasChartUpdate) {
+        this.updateBarChart();
+      } else {
+        if (newChartData?.length) {
+          this.createBarChart();
+          this.hasChartUpdate = true;
+        }
       }
     });
   }
 
-  private initializeChart() {
+  private createBarChart() {
+    this.chartSize.emit({ width: this.width, height: this.height });
+
+    this.svg = d3.select(this.barChartContainer.nativeElement)
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g');
+
+    this.x = d3.scaleBand()
+      .range([0, this.width])
+      .padding(0.1);
+
+    this.y = d3.scaleLinear()
+      .range([this.height, 0]);
+
+    this.updateBarChart();
+  }
+
+  private updateBarChart() {
+    if (!this.data) return;
+
+    this.x.domain(this.data().map(d => d.name));
+    this.y.domain([0, d3.max(this.data(), d => d.value)]);
+
+    const update = this.svg.selectAll('.bar')
+      .data(this.data);
+
+    update.enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d: { name: string; value: number }) => this.x(d.name))
+      .attr('width', this.x.bandwidth())
+      .attr('y', (d: { name: string; value: number }) => this.y(d.value))
+      .attr('height', (d: { name: string; value: number }) => this.height - this.y(d.value))
+      .merge(update)
+      .transition()
+      .duration(750)
+      .attr('x', (d: { name: string; value: number }) => this.x(d.name))
+      .attr('width', this.x.bandwidth())
+      .attr('y', (d: { name: string; value: number }) => this.y(d.value))
+      .attr('height', (d: { name: string; value: number }) => this.height - this.y(d.value))
+      .attr('fill', 'var(--mdc-switch-selected-focus-state-layer-color)');
+
+    update.exit()
+      .transition()
+      .duration(750)
+      .attr('y', (d: { name: string; value: number }) => this.y(d.value))
+      .attr('height', (d: { name: string; value: number }) => this.height - this.y(d.value))
+      .remove();
+  }
+
+  // - This method is to set the chart size fit to the data
+  private updateChartSizeByData() {
     // - barlength is the width of the bar
     const barLength = this.data().length < 5 ? this.data().length * 10 : 15;
 
@@ -41,31 +107,5 @@ export class BarChartComponent {
     // - width is the total width of the chart
     this.width = (this.data().length * xlength);
     this.height = (this.data().length * xlength);
-    this.chartSize.emit({ width: this.width, height: this.height });
-
-    const svg = d3.select(this.barChartContainer.nativeElement)
-      .append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height)
-
-    const x = d3.scaleBand().rangeRound([0, this.width - this.data().length]).domain(this.data().map((d) => d.name));
-
-    const xAxis = d3.axisBottom(x).tickValues([this.data()[0].name, this.data()[this.data().length - 1].name]);
-
-    // - append the x axis to the svg. this.height * 0.80 is the position of the x axis (80% of the height of the svg)
-    svg.append('g')
-      .attr('transform', `translate(0 ,${this.height * 0.80})`)
-      .call(xAxis);
-
-    // - append the bars to the svg. (this.height * 0.75) - d.value) is the position of the bars (75% of the height of the svg)
-    svg.selectAll('rect')
-      .data(this.data())
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * xlength)
-      .attr('y', (d) => (this.height * 0.75) - d.value)
-      .attr('width', barLength)
-      .attr('height', (d) => d.value)
-      .attr('fill', 'var(--mdc-switch-selected-focus-state-layer-color)');
   }
 }
